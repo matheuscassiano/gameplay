d0c54b227e661c08ad7b6f980e0f3621d2efca3c
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-24 11:22:43 -0300

    feat(SignIn): add styles

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,36 @@
 import styled from 'styled-components/native';
+import { theme } from '../../global/styles/theme';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 25px;
   align-items: center;
   justify-content: center;
+  background-color: ${theme.colors.secondary90};
+`;
+
+export const Content = styled.View`
+  margin-top: -40px;
+  padding: 0 50px;
+`;
+
+export const Cover = styled.Image.attrs({
+  source: require('../../assets/illustration.png'),
+  resizeMode: 'stretch'
+})`
+  width: 100%;
+  height: 360px;
+`;
+
+export const Title = styled.Text`
+  color: ${theme.colors.heading};
+  text-align: center;
+  font-size: 40;
+  margin-bottom: 16px;
+`;
+
+export const SubTitle = styled(Title)`
+  font-size: 15px;
+  margin-bottom: 64px;
 `;
 
 export const Input = styled.TextInput`
