5bdd3e7d9025ec48b0c1e4fff1396ac8f4009aa7
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-24 10:41:30 -0300

    feat(SingIn): add basic styled components

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 25px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Input = styled.TextInput`
+  width: 100%;
+`;
